04a099c84881e0918320183fd361a94e3c9735c4
kktavii <tavinhomito09@Gmail.com>
2025-08-25 17:25:03 -0300

    Update Aluno.java

--- src/com/java/sga/model/Aluno.java
+++ src/com/java/sga/model/Aluno.java
@@ -1,26 +1,25 @@
 package com.java.sga.model;
-import com.java.sga.util.Validacoes;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Aluno extends Pessoa {
     private String matricula;
-    private List<Double> notas;
-    
+
+    // Construtor
     public Aluno(String nome, String matricula) {
         super(nome);
-        setMatricula(matricula);
-        this.notas = new ArrayList<>();
+        this.matricula = matricula;
     }
 
-    
-    public void adicionarNota(double nota) {
-        if (Validacoes.isNotaValida(nota)) {
-            notas.add(nota);
-        }
+    // Getters e Setters
+    public String getMatricula() {
+        return matricula;
     }
-    
-    public double calcularMedia() {
-        return notas.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno {" + super.toString() + ", Matrícula: " + matricula + "}";
     }
 }
