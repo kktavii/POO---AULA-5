cd1bb0de8b9c829701c220b786bc8b11d1df7209
kktavii <tavinhomito09@Gmail.com>
2025-08-25 17:25:53 -0300

    Update Professor.java

--- src/com/java/sga/model/Professor.java
+++ src/com/java/sga/model/Professor.java
@@ -1,20 +1,37 @@
 package com.java.sga.model;
-import com.java.sga.util.Validacoes;
-// Contrutores
+
 public class Professor extends Pessoa {
     private String disciplina;
     private String turma;
-    
+
+    // Construtor
     public Professor(String nome, String disciplina, String turma) {
         super(nome);
-        setDisciplina(disciplina);
-        setTurma(turma);
+        this.disciplina = disciplina;
+        this.turma = turma;
     }
-    
-    // Getters e Setters 
+
+    // Getters e Setters
+    public String getDisciplina() {
+        return disciplina;
+    }
+
     public void setDisciplina(String disciplina) {
-        if (Validacoes.isDisciplinaValida(disciplina)) {
-            this.disciplina = disciplina;
-        }
+        this.disciplina = disciplina;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor {" + super.toString() + 
+               ", Disciplina: " + disciplina + 
+               ", Turma: " + turma + "}";
     }
 }
